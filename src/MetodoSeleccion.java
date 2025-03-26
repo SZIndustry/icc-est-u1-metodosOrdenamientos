4f4f142d3c3405ba1f1dfa85b943eb94eeaa45ba
sDucker <scri0962@gmail.com>
2025-03-25 18:45:38 -0600

    Clase.

--- src/MetodoSeleccion.java
+++ src/MetodoSeleccion.java
@@ -1,10 +1,9 @@
 public class MetodoSeleccion {
     
-    // Método para ordenar el arreglo en orden ascendente o descendente
     public void ordenar(int[] arreglo, boolean conditional) {
         if (conditional) {
             for (int i = 0; i < arreglo.length; i++) {
-                int iMin = i; // Índice del elemento extremo (mayor o menor según el orden)
+                int iMin = i; 
                 for (int j = i + 1; j < arreglo.length; j++) {
                     if (arreglo[j] < arreglo[iMin]) {
                         iMin  = j;
@@ -35,11 +34,10 @@ public class MetodoSeleccion {
         
     }
 
-    // Método para imprimir el arreglo
     public void printArray(int[] arreglo) {
         for (int i = 0; i < arreglo.length; i++) {
             System.out.print(arreglo[i] + " ");
         }
-        System.out.println(); // Salto de línea para mejor formato
+        System.out.println(); 
     }
 }
